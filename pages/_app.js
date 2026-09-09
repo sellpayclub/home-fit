8469591c49b05dc6e19fdfe3ff41ff72477c855f
danielpaixaog <117575434+sellpayclub@users.noreply.github.com>
2026-09-09 10:59:00 -0300

    Atualização do projeto via CodAí

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://checkout.sellpay.com.br/utm-link-bridge.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
