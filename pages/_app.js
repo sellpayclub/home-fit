245bf76269e7b8194ee720554d032fceb489e544
danielpaixaog <117575434+sellpayclub@users.noreply.github.com>
2026-09-10 09:08:02 -0300

    Atualização do projeto via CodAí

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,18 @@ export default function App({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18406188810"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18406188810');
+        `}
+      </Script>
+      <Script
         src="https://checkout.sellpay.com.br/utm-link-bridge.js"
         strategy="afterInteractive"
       />
